1adfe228624eeb9b7ba3cc6e76ee3bff6c9e26c0
kuramochi <kura19971106@gmail.com>
2026-07-01 20:44:23 +0900

    add coord_compression lib

---
+++ coord_compression.h
@@ -0,0 +1,14 @@
+// return {compressed vector, original values}
+// xs[compressed[i]] == vec[i]
+pair<vector<int>, vector<long long>> coord_compression(const vector<long long>& vec) {
+    vector<long long> xs = vec;
+    sort(xs.begin(), xs.end());
+    xs.erase(unique(xs.begin(), xs.end()), xs.end());
+
+    vector<int> compressed(vec.size());
+    for (int i = 0; i < (int)vec.size(); i++) {
+        compressed[i] = lower_bound(xs.begin(), xs.end(), vec[i]) - xs.begin();
+    }
+
+    return {compressed, xs};
+}

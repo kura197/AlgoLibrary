d99e1f9e53ccfd676a306d90608f7387ca8d3f8f
kuramochi <kura19971106@gmail.com>
2026-07-01 15:15:42 +0900

    update gcd.h

--- gcd.h
+++ gcd.h
@@ -1,53 +1,13 @@
-
-ll gcd(ll a, ll b){
-    if(b == 0) return a;
-    return gcd(b,a % b);
-}
-
-// ABC186 E
-// O(log max(a, b) ?)
-// ax + by = gcd(a, b)を満たす(x, y)
-ll extGCD(ll a, ll b, ll& x, ll& y){
+// 拡張Euclidの互除法 O(log max(a, b))
+// ax + by = gcd(a, b) を満たす x, y と gcd(a, b) を返す
+long long extGCD(long long a, long long b, long long& x, long long& y){
     if(b == 0){
-        // dx + 0y = d
         x = 1;
         y = 0;
         return a;
     }
-    else{
-        ll d = extGCD(b, a%b, y, x);
-        y -= (a/b) * x;
-        return d;
-    }
-}
 
-
-//// ax + by = d の一般解
-ll g = gcd(a, b);
-if(d % g != 0){
-    // 解なし
-    return -1;
-}
-else{
-    ll tmpx, tmpy;
-    extGCD(a, b, tmpx, tmpy);
-    x = b / g + tmpx;
-    y = a / g + tmpy;
-}
-
-
-// 負の数に対応したmod
-inline ll mod(ll a, ll m){
-    return (a % m + m) % m;
-}
-
-// aの逆元を求める。(gcd(a, m) == 1)
-// ax - my = 1
-ll mod_inv(ll a, ll m){
-    ll x, y;
-    extGCD(a, m, x, y);
-    //y *= -1;
-    //ll inv = m * ((-x + m - 1)/m) + x;
-    ll inv = mod(x, m);
-    return inv;
+    long long d = extGCD(b, a % b, y, x);
+    y -= (a / b) * x;
+    return d;
 }

6886ced08c1d9c123df4cf95e89e90c590c9e5b2
kuramochi <kura19971106@gmail.com>
2026-09-13 09:33:24 +0900

    change to linear

--- library/sieve.h
+++ library/sieve.h
@@ -5,8 +5,8 @@
 #include <map>
 #include <vector>
 
-// エラトステネスの篩
-// 構築 O(n log log n), 素因数分解 O(log n), 約数列挙 O(約数個数)
+// 線形篩
+// 構築 O(n), 素因数分解 O(log n), 約数列挙 O(約数個数)
 struct Sieve {
     int built_n = -1;
     // primes[i] := i番目の素数
@@ -26,31 +26,36 @@ struct Sieve {
     Sieve() = default;
     Sieve(int n) { build(n); }
 
-    // n以下について素数情報を前計算する O(n log log n)
+    // n以下について素数情報を前計算する O(n)
     void build(int n) {
         built_n = n;
         primes.clear();
-        is_prime.assign(n + 1, true);
+        is_prime.assign(n + 1, false);
         min_prime.assign(n + 1, -1);
         omega.assign(n + 1, 0);
         mobius.assign(n + 1, 1);
 
-        if (n >= 0) is_prime[0] = false;
-        if (n >= 1) is_prime[1] = false;
-
         for (int i = 2; i <= n; i++) {
-            if (!is_prime[i]) continue;
-            primes.push_back(i);
-            min_prime[i] = i;
-            omega[i] = 1;
-            mobius[i] = -1;
+            if (min_prime[i] == -1) {
+                primes.push_back(i);
+                is_prime[i] = true;
+                min_prime[i] = i;
+                omega[i] = 1;
+                mobius[i] = -1;
+            }
 
-            for (int j = 2 * i; j <= n; j += i) {
-                is_prime[j] = false;
-                omega[j]++;
-                if (min_prime[j] == -1) min_prime[j] = i;
-                if ((j / i) % i == 0) mobius[j] = 0;
-                else mobius[j] = -mobius[j];
+            for (int prime : primes) {
+                long long composite = (long long)i * prime;
+                if (composite > n) break;
+
+                min_prime[composite] = prime;
+                if (i % prime == 0) {
+                    omega[composite] = omega[i];
+                    mobius[composite] = 0;
+                    break;
+                }
+                omega[composite] = omega[i] + 1;
+                mobius[composite] = -mobius[i];
             }
         }
     }
